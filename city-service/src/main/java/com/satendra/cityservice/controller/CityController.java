aea1939d469b0a1906fececb7c9cd8cf4074d5e8
satdrasing <satendra.kohli@gmail.com>
2019-11-23 16:14:54 +0530

    added load blance in uua

--- city-service/src/main/java/com/satendra/cityservice/controller/CityController.java
+++ city-service/src/main/java/com/satendra/cityservice/controller/CityController.java
@@ -1,21 +1,16 @@
 package com.satendra.cityservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
 import java.util.Collection;
 import java.util.List;
 
 @RestController
 public class CityController {
 
-    @Autowired
-    private DataSource dataSource;
     @GetMapping("city")
     public Collection<String> getAllCity(){
-        System.out.println(dataSource);
         return List.of("Mumbai", "Delhi","Bangalore","Hyderabad","Ahmedabad","Chennai");
     }
 }
