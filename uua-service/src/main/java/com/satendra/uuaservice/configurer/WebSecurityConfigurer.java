82a368de9735e1b34975ed1005892807bd80ff37
satdrasing <satendra.kohli@gmail.com>
2019-12-17 22:36:08 +0530

    docker added

--- uua-service/src/main/java/com/satendra/uuaservice/configurer/WebSecurityConfigurer.java
+++ uua-service/src/main/java/com/satendra/uuaservice/configurer/WebSecurityConfigurer.java
@@ -4,18 +4,19 @@ package com.satendra.uuaservice.configurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
--- uua-service/src/main/resources/data.sql
+++ uua-service/src/main/resources/data.sql
@@ -27,6 +27,34 @@ values
   );
 
 
+insert into OAUTH_CLIENT_DETAILS (
+  CLIENT_ID,
+  RESOURCE_IDS,
+  CLIENT_SECRET,
+  SCOPE,
+  AUTHORIZED_GRANT_TYPES,
+  WEB_SERVER_REDIRECT_URI,
+  AUTHORITIES,
+  ACCESS_TOKEN_VALIDITY,
+  REFRESH_TOKEN_VALIDITY,
+  ADDITIONAL_INFORMATION,
+  AUTOAPPROVE
+)
+values
+  (
+    'client-1',
+     null,
+     '$2a$10$cThY1RErTtXeFTrFDITn4up.cxSM4ajFHVQ.P88W2h6PlF94aO.VS',
+    'read,write',
+    'authorization_code',
+    'http://localhost:8080/welcome',
+    null,
+    120,
+    240,
+    null,
+    true
+  );
+
   -- User user/pass
 INSERT INTO users (username, password, enabled)
   values ('satendra',
